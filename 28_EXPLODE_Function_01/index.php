878fdf7d5f2d58bd4e01ee069aab089fd597901b
Shubham <admin@itsmeshubham.com>
2020-11-16 15:28:06 +0530

    Added more Example Lines

--- 28_EXPLODE_Function_01/index.php
+++ 28_EXPLODE_Function_01/index.php
@@ -8,6 +8,16 @@
 $a_line = "Hello im shubham";
 $a_line_arr = EXPLODE("a", $a_line);
 print_r($a_line_arr);
+
+// ----------------
+/*
+$dt = '2009-01-16';
+
+$m =explode('-',$dt)[1];
+$d =explode('-',$dt)[2];
+$y =explode('-',$dt)[0];
+*/
+
 ?>
 </body>
 </html>
