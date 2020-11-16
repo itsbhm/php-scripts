f04aaa44b4e50f5591b4abf593d0309580fa0f6e
Shubham <admin@itsmeshubham.com>
2020-11-16 15:26:46 +0530

    Revert Changes

--- 29_IMPLODE_Function/index.php
+++ 29_IMPLODE_Function/index.php
@@ -10,16 +10,6 @@ $s_data = IMPLODE("_", $a_line);
 //print_r($a_line);
 echo $s_data;
 
-
-// ----------------
-/*
-$dt = '2009-01-16';
-
-$m =explode('-',$dt)[1];
-$d =explode('-',$dt)[2];
-$y =explode('-',$dt)[0];
-*/
-
 ?>
 </body>
 </html>
